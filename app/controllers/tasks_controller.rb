819581059609e9930f8f749b3a1eec41878af193
yasuhiro.sakano <yasuhiro7499@gmail.com>
2018-05-16 00:08:53 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,10 @@
 class TasksController < ApplicationController
   before_action :set_task,only: [:show, :edit, :update, :destroy]
-  
- def index
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+ 
+ 
+def index
   @tasks= Task.all
   end
 
@@ -14,14 +17,15 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      render 'toppages/index'
     end
   
   end
@@ -44,16 +48,13 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
-  
+    redirect_back(fallback_location: root_path)
   end
   
   
     private
 
-  # Strong Parameter
   
   def set_task
     @task= Task.find(params[:id])
@@ -63,5 +64,11 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
   
 end
